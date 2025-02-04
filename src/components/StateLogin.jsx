eb065a4937383274b2a40c71ebe275b254cec3f2
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-03 22:37:40 -0600

    state login and display email is invalid

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -7,7 +7,7 @@ export default function StateLogin() {
     email: "",
     password: "",
   });
-  const emailIsInvalid = enteredValues.email.includes('@')
+  const emailIsInvalid = !enteredValues.email.includes('@');
   function handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted");
@@ -39,6 +39,9 @@ export default function StateLogin() {
             onChange={() => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
