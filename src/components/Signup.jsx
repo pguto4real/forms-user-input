52f94390d0673740d2d77a7e3d58bad163434adf
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-05 23:24:11 -0600

    role required

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -55,7 +55,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
