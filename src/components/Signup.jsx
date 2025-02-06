7bf196d132e0267226ee9e9bab2eaf34f7d07e01
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-05 23:24:50 -0600

    terms-and-conditions required

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -94,7 +94,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
           agree to the terms and conditions
         </label>
       </div>
