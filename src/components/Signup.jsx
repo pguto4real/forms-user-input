cc656fe60e692562400f0c8d5885dcf8dd619559
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-05 23:20:36 -0600

    using required

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -14,7 +14,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" req/>
+        <input id="email" type="email" name="email" required/>
       </div>
 
       <div className="control-row">
